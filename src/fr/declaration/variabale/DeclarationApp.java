6117a03f48dd7d5c547e29ffa5059ca049e71db6
ElodieWDJ <56838354+ElodieWDJ@users.noreply.github.com>
2020-06-24 13:45:29 +0200

    V1

--- src/fr/declaration/variabale/DeclarationApp.java
+++ src/fr/declaration/variabale/DeclarationApp.java
@@ -1,5 +1,22 @@
 package fr.declaration.variabale;
 
 public class DeclarationApp {
+	
+	public static void main(String[]args) {
+		
+		byte b = 5;
+		short s = -20555;
+		int i = 3000555;
+		long  l = 20000L;
+		float f = 5.5F;
+		double d = 5.5;
+		char c = 'e';
+		boolean myBool = true;
+		String s1 = "Elodie"; 
+		System.out.println("Coucou");
+		
+		String randomString= "Voici le résultat d’un calcul '\n' 1+5=6";
+		System.out.println(randomString);
+	}
 
 }
